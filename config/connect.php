c26ad825497f2c2335b335c2d622afd4ef766047
Trí Quỳnh <triquynh1907@gmail.com>
2024-10-27 03:38:15 +0700

    Delete config directory

--- config/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","mohinhnv");
-
-// Check connection
-if ($mysqli->connect_errno) {
-  echo "Loi Ket NoiMySQL: " . $mysqli->connect_error;
-  exit();
-}
-?>
